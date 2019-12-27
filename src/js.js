028d91106a930f626e3a7f14d6d1764c6ebf3989
Dimo Karaivanov <doftor.livain@gmail.com>
2019-12-27 16:53:45 +0200

    slight js reorganization

--- src/js.js
+++ src/js.js
@@ -1,13 +1,44 @@
-function toggleClass(selector, className) {
-	const $element = typeof selector === 'object' ? selector : document.querySelector(selector);
+function selectElement(selector) {
+	return typeof selector === 'object' ? selector : document.querySelector(selector);
+}
+
+
+function hasClass(selector, className) {
+	const $element = selectElement(selector);
 	if (!$element) {
-		return;
+		return false;
 	}
 
-	if ($element.className.indexOf(className) === -1) {
-		$element.className = $element.className.concat(` ${className}`);
-	} else {
-		$element.className = $element.className.replace(className, '');
+	return $element.className.indexOf(className) !== -1;
+}
+
+
+function removeClass(selector, className) {
+	const $element = selectElement(selector);
+	if (!$element) {
+		return false;
+	}
+
+	$element.className = $element.className.replace(className, '');
+
+	return true;
+}
+
+
+function addClass(selector, className) {
+	const $element = selectElement(selector);
+	if (!$element || hasClass(selector, className)) {
+		return false;
+	}
+
+	$element.className = $element.className.concat(` ${className}`);
+	return true;
+}
+
+
+function toggleClass(selector, className) {
+	if (!addClass(selector, className)) {
+		removeClass(selector, className);
 	}
 }
 
@@ -17,5 +48,14 @@ function togglePlaylist() {
 }
 
 function toggleMute() {
+	$element = selectElement('.player-controls .volume')
+	if (hasClass($element, 'fa-volume-up')) {
+		removeClass($element, 'fa-volume-up');
+		addClass($element, 'fa-volume-mute');
+	} else {
+		removeClass($element, 'fa-volume-mute');
+		addClass($element, 'fa-volume-up');
+	}
+
 	console.log('toggle volume');
 }
