156416499dc8ff665f7fb3902441f26b4d791758
Dimo Karaivanov <doftor.livain@gmail.com>
2020-01-18 18:52:46 +0200

    album view

--- src/py/music/urls.py
+++ src/py/music/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('музика/', views.дискография, name='дискография'),
     path('music/', views.discography, name='discography'),
     path('musique/', views.discographie, name='discographie'),
+
+    path('музика/албуми/', views.албум, name='албум'),
+    path('music/albums/', views.album_en, name='album_en'),
+    path('musique/albums/', views.album_fr, name='album_fr'),
 ]
--- src/py/music/views.py
+++ src/py/music/views.py
@@ -3,13 +3,18 @@ from django.shortcuts import render, redirect
 from django.utils.translation import gettext, activate, get_language
 
 
-def render_discography(request):
-    language = get_language()
-
-    response = render(
-        request,
-        'music/discography.html',
-        {
+def render_albums(request, single=False):
+    if single:
+        context = {
+            'page': {
+                'url': 'music/albums/[[ALBUM_URL]]',
+                'title': '[[ALBUM NAME]]',
+                'description': '[[ALBUM NAME]]. %s ' % gettext('Track list and information.'),
+                'single_album': True,
+            }
+        }
+    else:
+        context = {
             'page': {
                 'url': 'music/',
                 # Translators: Discography page title
@@ -18,22 +23,38 @@ def render_discography(request):
                 'description': gettext('Dimmu Saurus Discography'),
             }
         }
-    )
 
-    response.set_cookie(settings.LANGUAGE_COOKIE_NAME, language)
+    response = render(request, 'music/discography.html', context)
+
+    response.set_cookie(settings.LANGUAGE_COOKIE_NAME, get_language())
     return response
 
 
 def дискография(request):
     activate('bg')
-    return render_discography(request)
+    return render_albums(request)
 
 
 def discography(request):
     activate('en')
-    return render_discography(request)
+    return render_albums(request)
 
 
 def discographie(request):
     activate('fr')
-    return render_discography(request)
+    return render_albums(request)
+
+
+def албум(request):
+    activate('bg')
+    return render_albums(request, True)
+
+
+def album_en(request):
+    activate('en')
+    return render_albums(request, True)
+
+
+def album_fr(request):
+    activate('fr')
+    return render_albums(request, True)
