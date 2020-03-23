9f7aa94e46e28667d4727f4577e288ca74896510
Dimo Karaivanov <doftor.livain@gmail.com>
2020-03-23 16:38:14 +0200

    audio-converter now uses fdkaac for improved aac quality

--- build-tools/audio-convert.sh
+++ build-tools/audio-convert.sh
@@ -6,7 +6,8 @@ format=$3
 
 if ! [[ -d $input_dir && -d $output_dir ]] || [[ $format != 'opus' && $format != 'aac' ]]
 then
-	echo 'Converts a directory of .flac files to .opus/.aac. Requires faac, flac and ffmpeg to be installed.'
+	echo 'Converts a directory of .flac files to .opus/.aac. Requires fdkaac, flac and ffmpeg to be installed.'
+	echo 'Note: It is an input directory, not dir/*.flac'
 	echo 'Usage:'
 	echo "  $0 INPUT_DIR OTPUT_DIR aac|opus"
 	exit 42
@@ -24,11 +25,13 @@ fi
 
 if [[ $format == 'aac' ]]
 then
-	cd $input_dir
 	for f in "$input_dir"/*.flac
 	do
 		filename=`basename "$f"`
 		filename="${filename%.flac}"
-		ffmpeg -i "$f" -c:a aac -b:a 160k -movflags +faststart "$output_dir"/"$filename".mp4
+		ffmpeg -i "$f" -ar 44100 -sample_fmt s16 -f wav -acodec pcm_s16le -ac 2 - | fdkaac --ignorelength --profile 2 --bitrate-mode 4 -o "$output_dir"/__tmp.m4a -
+		ffmpeg -i "$f" -i "$output_dir"/__tmp.m4a -map 1 -c copy -map_metadata 0 -map_metadata:s:a 0:s:a -movflags +faststart "$output_dir"/"$filename".m4a
+
+		rm "$output_dir"/__tmp.m4a
 	done
 fi
