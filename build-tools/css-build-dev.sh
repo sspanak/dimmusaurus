5bdc3a28d5936905691c9eaa0d69ba329de82b5b
Dimo Karaivanov <doftor.livain@gmail.com>
2020-04-12 12:13:32 +0300

    added support for XXS screens

--- build-tools/css-build-dev.sh
+++ build-tools/css-build-dev.sh
@@ -7,12 +7,12 @@ then
 fi
 
 # geometry
-cat src/css/{normalize.css,[1-9]*.css} > ui-demo/ds.all.css
+cat src/css/{normalize.css,[0-8]*.css} > ui-demo/ds.all.css
 node build-tools/css-convert-legacy.js ui-demo/ds.all.css > ui-demo/ds.css
 rm ui-demo/ds.all.css
 
 #colors
-cat src/css/00-colors.css > ui-demo/ds.colors.css
+cat src/css/[9]*colors*.css > ui-demo/ds.colors.css
 cat ui-demo/ds.colors.css >> ui-demo/ds.css
 node build-tools/css-convert-legacy.js ui-demo/ds.colors.css > ui-demo/ds.legacy.css
 rm ui-demo/ds.colors.css
