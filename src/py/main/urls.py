05242e8f921a1479340a3c19f306b47ed9ede514
Dimo Karaivanov <doftor.livain@gmail.com>
2020-05-16 18:16:13 +0300

    fixed the the trailing slashes URL mayhem (hopefully)

--- src/py/main/urls.py
+++ src/py/main/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    re_path(r'(?:api/)?начало/', views.начало, name='начало'),
-    re_path(r'(?:api/)?home/', views.home, name='home'),
-    re_path(r'(?:api/)?accueil/', views.accueil, name='accueil'),
+    re_path(r'^(?:api/)?начало/$', views.начало, name='начало'),
+    re_path(r'^(?:api/)?home/$', views.home, name='home'),
+    re_path(r'^(?:api/)?accueil/$', views.accueil, name='accueil'),
     path('version/', views.version, name="version")
 ]
--- src/py/music/urls.py
+++ src/py/music/urls.py
@@ -1,22 +1,23 @@
 from django.urls import path, re_path
+from django.views.generic.base import RedirectView
 
 from . import views
 
 urlpatterns = [
     # All albums
-    re_path(r'(?:api/)?музика/$', views.дискография, name='дискография'),
-    re_path(r'(?:api/)?music/$', views.discography, name='discography'),
-    re_path(r'(?:api/)?musique/$', views.discographie, name='discographie'),
+    re_path(r'^(?:api/)?музика/$', views.дискография, name='дискография'),
+    re_path(r'^(?:api/)?music/$', views.discography, name='discography'),
+    re_path(r'^(?:api/)?musique/$', views.discographie, name='discographie'),
 
     # Single album
-    re_path(r'^(?:api/)?музика/албуми/(?P<album_id>[^\-^/]+)', views.албум, name='албум'),
-    re_path(r'^(?:api/)?music/albums/(?P<album_id>[^\-^/]+)', views.album_en, name='album_en'),
-    re_path(r'^(?:api/)?musique/albums/(?P<album_id>[^\-^/]+)', views.album_fr, name='album_fr'),
+    re_path(r'^(?:api/)?музика/албуми/(?P<album_id>\d+)[^/]*/$', views.албум, name='албум'),
+    re_path(r'^(?:api/)?music/albums/(?P<album_id>\d+)[^/]*/$', views.album_en, name='album_en'),
+    re_path(r'^(?:api/)?musique/albums/(?P<album_id>\d+)[^/]*/$', views.album_fr, name='album_fr'),
 
     # Song
-    re_path(r'^(?:api/)?музика/песни/(?P<song_id>\d+)[^\/]*\/*$', views.песен, name='песен'),
-    re_path(r'^(?:api/)?music/songs/(?P<song_id>\d+)[^\/]*\/*$', views.song, name='song'),
-    re_path(r'^(?:api/)?musique/chansons/(?P<song_id>\d+)[^\/]*\/*$', views.chanson, name='chanson'),
+    re_path(r'^(?:api/)?музика/песни/(?P<song_id>\d+)[^\/]*\/$', views.песен, name='песен'),
+    re_path(r'^(?:api/)?music/songs/(?P<song_id>\d+)[^\/]*\/$', views.song, name='song'),
+    re_path(r'^(?:api/)?musique/chansons/(?P<song_id>\d+)[^\/]*\/$', views.chanson, name='chanson'),
 
     # Lyrics
     re_path(r'^(?:api/)?музика/песни/(?P<song_id>\d+)[^\/]*/текст/$', views.текст, name='текст'),
@@ -30,8 +31,21 @@ urlpatterns = [
     re_path(r'^(?:api/)?musique/chansons/(?P<song_id>\d+)[^\/]*/telecharger/$', views.download, name='download'),
 
     # non-existing but meaninful URLs
-    re_path(r'^(?:музика/песни|music/songs|musique/chansons)/*$', views.index, name='music_index'),
-    re_path(r'^(?:музика/албуми|music/albums|musique/albums)/*$', views.index, name='music_index'),
+    re_path(
+        r'^(?:музика/песни|музика/албуми)/*$',
+        RedirectView.as_view(url='/музика/'),
+        name='дискография'
+    ),
+    re_path(
+        r'^(?:music/songs|music/albums)/*$',
+        RedirectView.as_view(url='/music/'),
+        name='discography'
+    ),
+    re_path(
+        r'^(?:musique/chansons|musique/albums)/*$',
+        RedirectView.as_view(url='/musique/'),
+        name='discographie'
+    ),
 
     # Playlist (JSON only)
     path('api/music/playlist/bg/', views.плейлиста, name='playlist'),
