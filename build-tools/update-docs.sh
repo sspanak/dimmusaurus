41bf9e92ae91dd174c1ac343397eb0a83c2173a6
sspanak <doftor.livain@gmail.com>
2021-11-28 11:54:35 +0200

    automatic version management upon making the project

---
+++ build-tools/update-docs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if ! [[ -f Makefile && -d src && -d src/py ]]
+then
+	echo "Please run this script from the root project directory."
+	exit 1
+fi
+
+version=`build-tools/generate-version.sh short`
+sed -r -i "s/Version 7\.[0-9]+/Version $version/" README.md
+
+year=`date +"%Y"`
+sed -r -i "s/2004-2[0-9]{3}/2004-$year/" LICENSE.txt
+
