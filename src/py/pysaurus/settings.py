e7c40483e29a44fea4aa51ce35c3087c9d2718b0
sspanak <doftor.livain@gmail.com>
2021-12-23 10:43:45 +0200

    fixed the DEFAULT_AUTO_FIELD issue introduced after the Django upgrade

--- src/py/pysaurus/settings.py
+++ src/py/pysaurus/settings.py
@@ -96,6 +96,11 @@ DATABASES = {
     }
 }
 
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
