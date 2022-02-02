b8b742d96664e30dab6d011f5fc9c38015dbf58d
sspanak <doftor.livain@gmail.com>
2022-02-02 14:30:13 +0200

    fixed building of legacy css - it again includes only the important rules only

--- build-tools/css-build-dev.sh
+++ build-tools/css-build-dev.sh
@@ -7,11 +7,11 @@ then
 fi
 
 # geometry
-cat src/css/{normalize.css,[0-8]*.css} > ui-demo/ds.css
-node build-tools/css-convert-legacy.js ui-demo/ds.css > ui-demo/ds.legacy.css
+# the legacy rules are only a few, so we it's more efficient to include them in the main file
+cat src/css/{normalize.css,[0-8]*.css} | node build-tools/css-convert-legacy.js > ui-demo/ds.css
 
 # colors - legacy
-node build-tools/css-convert-legacy.js src/css/90-colors-white-stripes.css >> ui-demo/ds.legacy.css
+node build-tools/css-convert-legacy.js < src/css/90-colors-white-stripes.css > ui-demo/ds.legacy.css
 
 # colors - modern
 cat src/css/[9]*colors*.css >> ui-demo/ds.css
