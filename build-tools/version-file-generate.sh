5ce7347a00d79fb81bbcd45231e714df3cdfd69f
sspanak <doftor.livain@gmail.com>
2022-02-07 10:11:54 +0200

    stricter Makefile: if one of dependent commands fails, the following ones will not be executed, to prevent making a mess and for more meaningful error messages

--- build-tools/version-file-generate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if ! [[ -f Makefile && -d dist && -d dist/static ]]
-then
-	echo "Please run this script from the root project directory."
-	exit 1
-fi
-
-build-tools/generate-version.sh > dist/static/version.json
