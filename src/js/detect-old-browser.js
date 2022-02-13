35b8afbe69b2161e111005901140cddea8f2c45a
sspanak <doftor.livain@gmail.com>
2022-02-13 19:58:19 +0200

    better old and ancient browser detection for allowing modern CSS when possible

--- src/js/detect-old-browser.js
+++ src/js/detect-old-browser.js
@@ -1,14 +1,27 @@
-function isOldBrowser() {
+function isAncientBrowser() {
 	'use strict';
 
 	try {
 		if (!('userAgent' in navigator)) return false;
 		if (navigator.userAgent.indexOf('Edge') !== -1 && navigator.userAgent.match(/Edge\/(\d+)/)[1] < 16) return true;
+		if (!window.CSS.supports('(--var: 0)')) return true;
 		if (typeof Symbol === 'undefined') return true;
 		eval('class C {}');
-		eval('var a = (x) => x+1');
-		eval('for (const _ in []){}'); // Firefox < 52 / KaiOS 2.x cannot interpret "const" inside "for..in"
+		eval('var a=(x) => x+1');
+	} catch (e) {
+		return true;
+	}
+
+	return false;
+}
+
+function isOldBrowser() {
+	'use strict';
+
+	try {
 		if (!String.prototype.padStart || !Number.isNaN || !Number.parseInt || !Number.parseFloat) return true;
+		// Firefox < 52 / KaiOS 2.x cannot interpret "const" inside "for..in"
+		eval('for (const _ in []){}');
 	} catch (e) {
 		return true;
 	}
@@ -17,19 +30,15 @@ function isOldBrowser() {
 }
 
 function loadResources(jsPath, legacyJsPath, legacyCssPath) {
-	var js = document.createElement('script');
-	js.defer = 'defer';
-
-	if (isOldBrowser()) {
-		js.src = legacyJsPath;
-
+	if (isAncientBrowser()) {
 		var css = document.createElement('link');
 		css.rel = 'stylesheet';
 		css.href = legacyCssPath;
 		document.head.appendChild(css);
-	} else {
-		js.src = jsPath;
 	}
 
+	var js = document.createElement('script');
+	js.defer = 'defer';
+	js.src = isOldBrowser() ? legacyJsPath : jsPath;
 	document.head.appendChild(js);
 }
