8dd51bde871f72e92203f3876e2263bb61e9e522
sspanak <doftor.livain@gmail.com>
2022-02-28 15:17:07 +0200

    fixed the startup crash on ancient browsers, caused by theme autoselection

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,26 +1,31 @@
-// This file is included inline in order to apply the theme before rendering anything, so that
-// there would be no flash of unthemed content
+const Theme = new class { // eslint-disable-line no-unused-vars
+	constructor () {
+		this.themes = ['white-stripes', 'black-sabbath'];
 
-const THEMES = ['white-stripes', 'black-sabbath'];
-
-
-function getCurrentTheme() { // eslint-disable-line no-unused-vars
-	return localStorage.getItem('theme') || undefined;
-}
-
-
-function selectTheme(id) { // eslint-disable-line no-unused-vars
-	const theme = THEMES[id] || '';
-
-	let className = document.querySelector('body').getAttribute('class');
-	THEMES.forEach(t => className = className.replace(t, ''));
-	className = `${className} ${theme}`.replace(/\s+/, ' ');
-
-	document.querySelector('body').setAttribute('class', className);
-
-	if (theme) {
-		localStorage.setItem('theme', id);
-	} else {
-		localStorage.removeItem('theme');
+		// This file is included inline in order to apply the theme before rendering anything, so that
+		// there would be no flash of unthemed content
+		window.addEventListener('DOMContentLoaded', () => this.select(this.getCurrent()));
 	}
-}
+
+
+	getCurrent() {
+		return localStorage.getItem('theme') || undefined;
+	}
+
+
+	select(id) {
+		const theme = this.themes[id] || '';
+
+		let className = document.querySelector('body').getAttribute('class');
+		this.themes.forEach(t => className = className.replace(t, ''));
+		className = `${className} ${theme}`.replace(/\s+/, ' ');
+
+		document.querySelector('body').setAttribute('class', className);
+
+		if (theme) {
+			localStorage.setItem('theme', id);
+		} else {
+			localStorage.removeItem('theme');
+		}
+	}
+};
