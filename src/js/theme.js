7dfdd3640fff9e5c17bdab9edf6e4d7b43da2d09
sspanak <doftor.livain@gmail.com>
2022-03-03 15:29:51 +0200

    theme meta updates correctly when the theme changes

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,6 +1,15 @@
 const Theme = new class { // eslint-disable-line no-unused-vars
 	constructor () {
-		this.themes = ['white-stripes', 'black-sabbath'];
+		this.themes = [
+			{
+				kind: 'light',
+				name: 'white-stripes'
+			},
+			{
+				kind: 'dark',
+				name: 'black-sabbath'
+			}
+		];
 
 		// This file is included inline in order to apply the theme before rendering anything, so that
 		// there would be no flash of unthemed content
@@ -14,18 +23,45 @@ const Theme = new class { // eslint-disable-line no-unused-vars
 
 
 	select(id) {
-		const theme = this.themes[id] || '';
+		const { name, kind } = this.themes[id] || { kind: '' , name: '' };
 
+		this
+			._apply(name)
+			._setMeta(kind)
+			._save(name, id);
+	}
+
+
+	_apply(themeName) {
 		let className = document.querySelector('body').getAttribute('class');
-		this.themes.forEach(t => className = className.replace(t, ''));
-		className = `${className} ${theme}`.replace(/\s+/, ' ');
+		this.themes.forEach(t => className = className.replace(t.name, ''));
+		className = `${className} ${themeName}`.replace(/\s+/, ' ');
 
 		document.querySelector('body').setAttribute('class', className);
 
-		if (theme) {
+		return this;
+	}
+
+
+	_setMeta(themeKind) {
+		const $metaColorScheme = document.head.querySelector('meta[name=color-scheme]');
+		if (themeKind) {
+			$metaColorScheme.setAttribute('content', themeKind);
+		} else {
+			$metaColorScheme.setAttribute('content', 'light dark');
+		}
+
+		return this;
+	}
+
+
+	_save(themeName, id) {
+		if (themeName) {
 			localStorage.setItem('theme', id);
 		} else {
 			localStorage.removeItem('theme');
 		}
+
+		return this;
 	}
 };
